f36c3571dafae33a2b2ec2434c501757cf2cf716
Matt Clarke <matt.clarke@esss.se>
2020-03-10 15:14:01 +0100

    Use Kafka Cache

--- nicos_ess/essiip/setups/special/cache.py
+++ nicos_ess/essiip/setups/special/cache.py
@@ -1,21 +1,19 @@
-description = 'setup for the cache server'
-group = 'special'
+description = "setup for the cache server"
+group = "special"
 
 devices = dict(
     serializer=device(
-        'nicos.services.cache.entry.serializer.flatbuffers.FlatbuffersCacheEntrySerializer'),
-
-    DB=device(
-        'nicos.services.cache.database.kafka.KafkaCacheDatabaseWithHistory',
-        currenttopic='UTGARD_nicosCacheCompacted',
-        historytopic='UTGARD_nicosCacheHistory',
-        brokers=["kafka:9092"],
-        loglevel='info',
-        serializer='serializer'
+        "nicos.services.cache.entry.serializer.flatbuffers.FlatbuffersCacheEntrySerializer"
     ),
-    Server = device('nicos.services.cache.server.CacheServer',
-        db = 'DB',
-        server = '',
-        loglevel = 'info',
+    DB=device(
+        "nicos.services.cache.database.kafka.KafkaCacheDatabaseWithHistory",
+        currenttopic="UTGARD_nicosCacheCompacted",
+        historytopic="UTGARD_nicosCacheHistory",
+        brokers=["dmsc-kafka01.cslab.esss.lu.se:9092"],
+        loglevel="info",
+        serializer="serializer",
+    ),
+    Server=device(
+        "nicos.services.cache.server.CacheServer", db="DB", server="", loglevel="info"
     ),
 )
