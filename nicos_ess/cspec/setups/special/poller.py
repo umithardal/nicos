dd661bddf5ae87320d7559d92fb86f5287b10331
Matt Clarke <matt.clarke@esss.se>
2020-03-25 08:34:13 +0100

    Running the services in separate containers

--- nicos_ess/cspec/setups/special/poller.py
+++ nicos_ess/cspec/setups/special/poller.py
@@ -4,7 +4,7 @@ group = 'special'
 sysconfig = dict(
     # use only 'localhost' if the cache is really running on the same machine,
     # otherwise use the official computer name
-    cache = 'localhost'
+    cache = 'nicos-cache'
 )
 
 devices = dict(
